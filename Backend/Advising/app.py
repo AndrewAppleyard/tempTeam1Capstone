433c9e1af46eae3cf206f08633b7bea3569653b9
Sophia Praphan <spraph00@uafs.edu>
2025-10-28 22:44:23 -0500

    added chris's code to app.py

--- Backend/Advising/app.py
+++ Backend/Advising/app.py
@@ -1,14 +1,25 @@
 from flask import Flask, Blueprint, url_for
+from APIs.StudentAPI import bp as student_bp
+from APIs.TransferAPI import bp as transfer_bp
+from Advising.APIs import AdvisorAPI, AdminAPI, StudentAPI
+from extensions import jwt
+from flask_cors import CORS
 import os
 import sys
 
-
-from Advising.APIs import AdvisorAPI, AdminAPI, StudentAPI
-
-
-
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+
+    CORS(app)
+
+    app.config["JWT_SECRET_KEY"] = "e90$2kj@#dju78)"
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 3600
+
+    jwt.init_app(app)
+
+    app.register_blueprint(transfer_bp)
+    app.register_blueprint(student_bp)
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
