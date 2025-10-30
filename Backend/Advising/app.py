52d224712320c8e85c06ec01fcfc8e386e4d1f62
AndrewAppleyard <appleyardandrew0@gmail.com>
2025-10-29 20:35:23 -0500

    LDAP fixes

--- Backend/Advising/app.py
+++ Backend/Advising/app.py
@@ -28,6 +28,8 @@ def create_app():
     return app
 
 def runDev():
+    app = create_app()
+    
     app.run(host='0.0.0.0', port=5000)
 
 #app = create_app()
--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -15,8 +15,16 @@ sudo systemctl start docker
 
 sudo usermod -aG docker $USER
 # newgrp docker
-
 sudo systemctl restart docker
+
+sudo docker-compose up -d dirsrv
+
+echo "Waiting for 389 Directory Server to become ready..."
+until sudo docker exec uafs_ldap ldapsearch -x -H ldap://localhost:3389 -b "dc=UAFS,dc=COM" >/dev/null 2>&1; do
+  echo "Waiting for dirsrv..."
+  sleep 5
+done
+
 
 #sudo docker compose down -v
 echo "Starting Docker containers..."
