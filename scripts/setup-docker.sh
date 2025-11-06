e5c5a89a1f8360a20a98edebc24d9ac03a56df6f
Andrew Appleyard <114895673+AndrewAppleyard@users.noreply.github.com>
2025-11-05 18:19:08 -0600

    Update setup-docker.sh

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -19,12 +19,7 @@ sudo systemctl restart docker
 
 sudo docker-compose up -d dirsrv
 
- echo "Waiting for 389 Directory Server to become ready..."
- until sudo docker exec uafs_ldap ldapsearch -x -H ldap://localhost:3389 -b "dc=UAFS,dc=COM" >/dev/null 2>&1; do
-   echo "Waiting for dirsrv..."
-   sleep 5
- done
-
+sleep 15
 
 #sudo docker compose down -v
 echo "Starting Docker containers..."
