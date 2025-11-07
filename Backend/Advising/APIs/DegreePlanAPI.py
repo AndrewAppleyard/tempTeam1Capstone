b907f2f8de2900d8efcd056ae0af8b418ea492c9
AndrewAppleyard <appleyardandrew0@gmail.com>
2025-11-06 19:05:13 -0600

    adding TODOS

---
+++ Backend/Advising/APIs/DegreePlanAPI.py
@@ -0,0 +1,4 @@
+#TODO
+#Add API endpoints for the database
+#Add the agent implimentation for getting all the degree plans and add them to database
+#Add userclass for SQL saving
--- Backend/Advising/APIs/TransferAPI.py
+++ Backend/Advising/APIs/TransferAPI.py
@@ -18,7 +18,7 @@ parent_dir = os.path.join(current_dir, '..')
 sys.path.append(parent_dir)
 
 LDAP_HOST = "dirsrv"
-LDAP_PORT = 3389
+LDAP_PORT = 7389
 LDAP_USER = "cn=Directory Manager"
 LDAP_PASS = "andrewandrew"
 BASE_DN = "dc=UAFS,dc=COM"
