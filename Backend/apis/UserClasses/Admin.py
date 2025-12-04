430e3a9a4c93ce95359ec7afc4394ce32a1c93d8
spraph00 <spraph00@uafs.edu>
2025-12-04 15:45:25 -0600

    Revert "Merge branch 'main' into dev"
    This reverts commit 708a84b43b2a7bc2b0305a4b1dd8ef6b0c0d3bb6, reversing
    changes made to 636a4cccb918412f420e93f3753aba1f33b61cb2.

--- Backend/apis/UserClasses/Admin.py
+++
@@ -1,8 +0,0 @@
-from UserClasses import User, Student, Advisor
-
-class Admin(User.User):
-    studentList: list[Student.Student]
-    advisorList: list[Advisor.Advisor]
-
-    def __init__(self):
-        pass
