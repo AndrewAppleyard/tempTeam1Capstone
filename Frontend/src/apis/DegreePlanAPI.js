d1e612e5c082ee41e9afc1f60a2cc643f9f1683d
Username <rfarra00@uafs.edu>
2025-12-07 13:25:20 -0500

    Implemented Degree plan drop down for students

--- Frontend/src/apis/DegreePlanAPI.js
+++ Frontend/src/apis/DegreePlanAPI.js
@@ -21,6 +21,29 @@ export default{
     } catch(err){
         console.log('Degree Plan Retrieval Error ', err);
     }
-}
+},
+    async get_majors(){
+         try{
+        const response = await api.get('/DegreePlan/View');
+        
+        const degrees = response.data.degrees;
+        let majorOptions = []
+        const majors = new Set();
+
+        degrees.forEach(degreePlan => {
+        const major = degreePlan.degree;  // Or use `degreePlan.major` if that's the correct field
+        if (major) {
+          majors.add(major);  // Add the major to the Set
+        }
+
+      });
+
+        majorOptions = Array.from(majors);
+        console.log('Majors Retrieved ', majorOptions)
+        return majorOptions;
+        } catch(err){
+            console.log('Degree Plan Retrieval Error ', err);
+        }
+    }
 
 }
